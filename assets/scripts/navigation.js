71cf8e69a87e6663a1d9695a517131ff7a1e3e92
Saif Khan <saifkhichi96@gmail.com>
2021-04-29 16:13:26 +0200

    updates

--- assets/scripts/navigation.js
+++ assets/scripts/navigation.js
@@ -24,6 +24,27 @@ $(function() {
   console.log("Navigation setup complete!");
 });
 
+// Spread breadcrumbs
+var hardcodedCrumbs = $('.breadcrumbs').html()
+$('.breadcrumbs').html("<li class='breadcrumb-item'><a class='muted' href='/'>Home</a></li>")
+var pages = location.href.split("://")[1].split('#')[0].split('?')[0].split("/").filter(function(el) {
+  return el != "" && el != "index.html";
+});
+var url = ''
+for (var i = 1, len = pages.length; i < len; i++) {
+  var page = pages[i].split('.')[0].replace('-', ' ').replace('_', ' ')
+  url += '/' + pages[i]
+  var oldHtml = $('.breadcrumbs').html()
+  if (i == pages.length - 1) {
+    var newHtml = oldHtml
+  } else {
+    var newHtml = oldHtml + "<li class='breadcrumb-item'><a class='muted' href='" + url + "'>" + page + "</a></li>"
+  }
+  $('.breadcrumbs').html(newHtml);
+}
+var oldHtml = $('.breadcrumbs').html()
+$('.breadcrumbs').html(oldHtml + hardcodedCrumbs);
+
 $('#last-modified').html(document.lastModified);
 
 function back() {
